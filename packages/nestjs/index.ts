61fad85ab00a3150e012436c9e67537eebeee57e
Kasper Gammeltoft <kaspergam@bellsouth.net>
2024-12-04 10:27:12 -0500

    fix: exports from package

---
+++ packages/nestjs/index.ts
@@ -0,0 +1,5 @@
+export * from './src/task-scheduler.module';
+export * from './src/task-scheduler.service';
+export * from './src/task.interface';
+export * from './src/task.decorators';
+export { ConditionsProvider } from './src/conditions.provider';
