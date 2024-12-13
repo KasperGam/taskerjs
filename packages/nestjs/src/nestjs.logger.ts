4bb4ad525ec90eb6472f8da98b5b85ba0a814494
Kasper Gammeltoft <kaspergam@bellsouth.net>
2024-12-13 09:58:50 -0500

    fix: nest logger wrapper with custom log levels

--- packages/nestjs/src/nestjs.logger.ts
+++ packages/nestjs/src/nestjs.logger.ts
@@ -4,19 +4,76 @@ import { Logger } from '@optask/tasker';
 /**
  * Simple wrapper around the nestjs default logger, implementing the logger interface
  */
-export class NestLogger extends NativeNestLogger implements Logger {
+export class NestLogger implements Logger {
+  logLevel = 3;
+
+  private logger: NativeNestLogger;
+
+  constructor(
+    name?: string,
+    options?: {
+      timestamp?: boolean;
+    },
+  ) {
+    if (name) {
+      this.logger = new NativeNestLogger(name, options);
+    } else {
+      this.logger = new NativeNestLogger();
+    }
+
+    const envVar = process.env[`TASKER_LOG_LEVEL`];
+    const parsed = parseInt(envVar, 10);
+    if (parsed >= 0) {
+      this.logLevel = Math.min(parsed, 5);
+    }
+  }
+
+  fatal(...args: any[]) {
+    if (this.logLevel >= 0) {
+      this.logger.fatal(args);
+    }
+  }
+
+  error(...args: any[]) {
+    if (this.logLevel >= 1) {
+      this.logger.error(args);
+    }
+  }
+
+  warn(...args: any[]) {
+    if (this.logLevel >= 2) {
+      this.logger.warn(args);
+    }
+  }
+
+  log(...args: any[]) {
+    if (this.logLevel >= 3) {
+      this.logger.log(args);
+    }
+  }
+
   info(...args: any[]) {
-    // Just map calls to info -> log
-    this.log(args);
+    if (this.logLevel >= 3) {
+      this.logger.log(args);
+    }
+  }
+
+  debug(...args: any[]) {
+    if (this.logLevel >= 4) {
+      this.logger.debug(args);
+    }
   }
 
   trace(...args: any[]) {
-    // Just map calls to trace -> verbose
-    this.verbose(args);
+    if (this.logLevel >= 5) {
+      this.logger.verbose(args);
+    }
   }
 
   // Construct new logger with a different name
   child(name?: string) {
-    return new NestLogger(name);
+    const newLogger = new NestLogger(name);
+    newLogger.logLevel = this.logLevel;
+    return newLogger;
   }
 }
