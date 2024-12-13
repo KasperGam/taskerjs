cbbb607cd00ed78584df0b917eb84daac1a0deb2
Kasper Gammeltoft <kaspergam@bellsouth.net>
2024-12-13 09:42:13 -0500

    feat: add logging to redis storage

--- packages/storage-redis/src/redis.store.ts
+++ packages/storage-redis/src/redis.store.ts
@@ -1,4 +1,4 @@
-import { Store } from '@optask/tasker';
+import { ConsoleLogger, Logger, Store } from '@optask/tasker';
 import { Redis, RedisOptions } from 'ioredis';
 
 export interface RedisStoreOptions extends RedisOptions {
@@ -7,33 +7,50 @@ export interface RedisStoreOptions extends RedisOptions {
 
 export class RedisStore implements Store {
   private client: Redis;
+  logger: Logger = new ConsoleLogger();
 
   constructor(private readonly options: RedisStoreOptions) {}
 
   async has(key: string): Promise<boolean> {
+    this.logger.trace(
+      `Testing if key ${key} exists in store in namespace ${this.options.namespace}`,
+    );
     const newKey = this.getNamespacedKey(key);
     const client = this.getClient();
 
     const numTimes = await client.exists(newKey);
-    return numTimes > 0;
+    const hasKey = numTimes > 0;
+    this.logger.debug(
+      `Key ${newKey} ${hasKey ? 'exists' : 'does not exist'} in the store`,
+    );
+    return hasKey;
   }
 
   async get(key: string): Promise<string | null> {
+    this.logger.trace(
+      `Fetching key ${key} from store in namespace ${this.options.namespace}`,
+    );
     const newKey = this.getNamespacedKey(key);
     const client = this.getClient();
 
     const data = await client.get(newKey);
     if (!data) {
+      this.logger.debug(`Key ${newKey} not found in store`);
       return null;
     }
+    this.logger.debug(`Key ${newKey} fetched in store with data ${data}`);
     return data;
   }
 
   async set(key: string, data: string) {
+    this.logger.trace(
+      `Set key ${key} with data ${data} in store with namespace ${this.options.namespace}`,
+    );
     const newKey = this.getNamespacedKey(key);
     const client = this.getClient();
 
     await client.set(newKey, data);
+    this.logger.debug(`Set key ${newKey} with data ${data} in store`);
   }
 
   getClient(): Redis {
